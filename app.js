9cb8e29a5ec88bdb7097d0262bcbaeef8122e0cf
Vamci <Kalidasu.Surada@gmail.com>
2016-12-29 18:48:11 +0530

    intial checkin

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static('app'));
+
+app.get('/',function(req,res)
+{
+  console.log(__dirname);
+  res.sendfile(__dirname + '/app/index.html');
+});
+
+
+app.set('port',process.env.PORT || 2424);
+
+var server = app.listen(app.get('port'),function(){
+	console.log('Listening port' + server.address().port);
+
+});
---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp'),
+	server = require('gulp-develop-server');
+
+gulp.task('server:start',function(){
+	server.listen({path:'./app.js'});
+});
+
+gulp.task('server:restart',function(){
+	gulp.watch(['./app.js'],server.restart);
+});
+
+gulp.task('default',function(){
+	gulp.run('server:start');
+});]
