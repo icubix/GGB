7c108523ce3325f8ce058b401bcf6dd5065d7237
Vamci <Kalidasu.Surada@gmail.com>
2017-01-03 14:14:35 +0530

    modified css

--- app/route/route.js
+++ app/route/route.js
@@ -2,8 +2,32 @@ var app = angular.module('GGBApp',['ngRoute','GGBApp.loginCtrl']);
 console.log('test ing ckali');
 app.config(function($routeProvider) {
   $routeProvider
-  .when('/',{
+  .when('/login',{
     templateUrl:'views/login.html',
     controller:'loginCtrl'
+  })
+  .when('/user',{
+    templateUrl:'views/adduser.html',
+    controller:'userCtrl'
+  })
+  .when('/check',{
+    templateUrl:'views/check.html',
+    controller:'checkCtrl'
+  })
+  .when('/device',{
+    templateUrl:'views/additionaldevice.html',
+    controller:'deviceCtrl'
+  })
+  .when('/notification',{
+    templateUrl:'views/notification.html',
+    controller:'notificationCtrl'
+  })
+  .when('/product',{
+    templateUrl:'views/productdec.html',
+    controller:'productCtrl'
+  })
+  .when('/security',{
+    templateUrl:'views/securitydetails.html',
+    controller:'securityCtrl'
   })
 });
