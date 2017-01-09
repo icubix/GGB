63ce77b947de7f2667c221e9479fa5d40c4cfac1
Vamci <Kalidasu.Surada@gmail.com>
2017-01-09 21:11:28 +0530

    modified check controller and  changed index fiels

--- app/factory/CheckFactoryService.js
+++ app/factory/CheckFactoryService.js
@@ -2,7 +2,7 @@ var app = angular.module('GGBApp.chkFactory',['GGBApp.myApp']);
 app.factory('checkFactory', function($http,urls){
 return{
 	 SearchDevices: function (data) {
-	 
+	 		console.log(data);
             return $http({
                 url: urls.apiUrl +'device/GetDevices',
                 method: "POST",
